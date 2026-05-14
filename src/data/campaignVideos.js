7ea7e96a7cf55a3e7d6475141e45c26f9aca0a25
Emmanuel <gemegahprince9@gmail.com>
2026-05-14 09:35:08 +0000

    Add campaign videos to rotation

--- src/data/campaignVideos.js
+++ src/data/campaignVideos.js
@@ -13,5 +13,15 @@ export const CAMPAIGN_VIDEOS = [
     id: 't021_30bhUw',
     quote: '"You are not behind. You are human, and you are trying."',
     title: 'Accra Coded campaign story 3'
+  },
+  {
+    id: '-dIdXyBQtwY',
+    quote: '"You are allowed to pause and still be making progress."',
+    title: 'Accra Coded campaign story 4'
+  },
+  {
+    id: 'AwqxUYZDzb4',
+    quote: '"One small breath can change the direction of this moment."',
+    title: 'Accra Coded campaign story 5'
   }
 ];
